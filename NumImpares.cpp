e34eb7024cc5598180bd3b7a190782b5eac5d7ff
Tel36071486 <diego.mtz.ra@gmail.com>
2022-04-01 22:15:41 -0600

    Primer commit despues de solucionar los repositorios

--- NumImpares.cpp
+++ NumImpares.cpp
@@ -1,19 +1,23 @@
-// NumImpares.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    int x, n;
+
+    cout << "Ingrese un numero:" << endl;
+    cin >> n;
+
+    cout << "Los numeros impares son:" << endl;
+
+    for (x = 1;x < n;x += 2) {
+        cout << x << endl;
+    }
+
+    if (n % 2 == 0) {
+
+    }
+    else {
+        cout << n << endl;
+    }
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
